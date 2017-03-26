427dd2cd690b6b270bd6760cffbc470271beef37
Jarred Capellman <jcapellman@hotmail.com>
2017-03-25 21:00:24 -0500

    Added: Initial Commit

---
+++ src/Source/Fatalus/Fatalus.h
@@ -0,0 +1,9 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#ifndef __FATALUS_H__
+#define __FATALUS_H__
+
+#include "EngineMinimal.h"
+
+
+#endif
---
+++ src/Source/Fatalus/FatalusGameMode.h
@@ -0,0 +1,16 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+#pragma once
+#include "GameFramework/GameModeBase.h"
+#include "FatalusGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AFatalusGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AFatalusGameMode();
+};
+
+
+
